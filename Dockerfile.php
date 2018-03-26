0b5a86592abfceaa97527bfdc940e56069588fc2
Rootrl <danhoo@163.com>
2018-03-26 18:45:39 +0800

    Dockfile modified

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,22 +1,8 @@
 FROM php:7.2-fpm
 
-RUN apt-get update --fix-missing && apt-get install -y \
-    g++ autoconf bash git apt-utils libxml2-dev libcurl3-dev pkg-config \
-    && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
+    Run  ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
     && echo "Asia/Shanghai" > /etc/timezone \
-    && docker-php-ext-install iconv curl mbstring \
-        xml json mcrypt mysqli pdo pdo_mysql zip \
-    && docker-php-ext-configure gd \
-        --with-gd \
-        --with-freetype-dir=/usr/include/ \
-        --with-png-dir=/usr/include/ \
-        --with-jpeg-dir=/usr/include/ \
-    && docker-php-ext-install gd \
-    && docker-php-ext-enable gd \
-    && pecl install /pecl/redis-3.0.0.tgz \
+    && docker-php-ext-install mysqli pdo pdo_mysql \
+    && pecl install redis \
     && pecl install swoole \
-    && docker-php-ext-enable redis swoole \
-    && apt-get purge -y --auto-remove \
-    && rm -rf /var/cache/apt/* \
-    && rm -rf /var/lib/apt/lists/* \
-    && rm -rf /pecl
+    && docker-php-ext-enable redis swoole
