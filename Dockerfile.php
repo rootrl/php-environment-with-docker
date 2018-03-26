2aa6bc9c8e6aaa86ca19c1b52336ea971ccfd6e9
Rootrl <danhoo@163.com>
2018-03-26 15:22:52 +0800

    Dockerfile modified

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,5 +1,21 @@
 FROM php:7.2-fpm
-RUN pecl install redis \
-    && pecl install xdebug \
-    && pecl install swoole \
-    && docker-php-ext-enable redis xdebug swoole
+
+RUN apt-get update --fix-missing && apt-get install -y \
+    g++ autoconf bash git apt-utils libxml2-dev libcurl3-dev pkg-config \
+    && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
+    && echo "Asia/Shanghai" > /etc/timezone \
+    && docker-php-ext-install iconv curl mbstring \
+        xml json mcrypt mysqli pdo pdo_mysql zip \
+    && docker-php-ext-configure gd \
+        --with-gd \
+        --with-freetype-dir=/usr/include/ \
+        --with-png-dir=/usr/include/ \
+        --with-jpeg-dir=/usr/include/ \
+    && docker-php-ext-install gd \
+    && docker-php-ext-enable gd \
+    && pecl install /pecl/redis-3.0.0.tgz \
+    && docker-php-ext-enable redis \
+    && apt-get purge -y --auto-remove \
+    && rm -rf /var/cache/apt/* \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm -rf /pecl
