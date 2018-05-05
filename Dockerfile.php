df2f267e31adfa75b642f8388a496ba06995056e
rootrl <danhoo@163.com>
2018-05-06 05:53:59 +0800

    Php gd ext support

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,8 +1,15 @@
 FROM php:7.2-fpm
+    Run echo "nameserver 223.5.5.5" >> /etc/resolv.conf \
+    && echo "nameserver 223.6.6.6" >> /etc/resolve.conf \
+    && apt-get update \
+    && apt-get install -y \
+        libfreetype6-dev \
+        libjpeg62-turbo-dev \
+        libpng-dev \
 
-    Run  ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
-    && echo "Asia/Shanghai" > /etc/timezone \
-    && docker-php-ext-install mysqli pdo pdo_mysql \
-    && pecl install redis \
+    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+    && docker-php-ext-install -j$(nproc) gd \
+    && docker-php-ext-install mysqli pdo_mysql \
     && pecl install swoole \
-    && docker-php-ext-enable redis swoole
+    && pecl install redis \
+    && docker-php-ext-enable swoole redis
